8d4fe8506b7c310f5edfd49db66ae7bc1d6561d7
DenisAnufriev <wenomavel89@gmail.com>
2024-09-22 20:18:20 +0300

    dev1 full commit

--- blog/templatetags/article_tags.py
+++ blog/templatetags/article_tags.py
@@ -9,3 +9,4 @@ def truncate_chars(value, max_chars=100):
     if len(value) > max_chars:
         return f"{value[:max_chars]}..."
     return value
+
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 app_name = BlogConfig.name
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
     path("view/<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
     path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("edit/<int:pk>", ArticleUpdateView.as_view(), name="update_article"),
+    path("delete/<int:pk>", ArticleDeleteView.as_view(), name="delete_article"),
 ]
