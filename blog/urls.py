b6acc62377a107ec03f9c8f8a20e17042acb6f8e
DenisAnufriev <wenomavel89@gmail.com>
2024-09-22 18:28:15 +0300

    update

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import ArticleListView, ArticleDetailView, ArticleCreateView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('', ArticleListView.as_view(), name='article_list'),
+    path("view/<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+]
